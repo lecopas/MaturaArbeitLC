5fb7d0e4fe6273e53c64defe3c813f9664dcf94e
Logan <logan.copas@outlook.com>
2024-06-14 16:39:46 +0200

    accelerators are now able to be dragged and become the child of a modifiable object

--- Assets/Scripts/DragnDrop.cs
+++ Assets/Scripts/DragnDrop.cs
@@ -7,6 +7,13 @@ public class DragnDrop : MonoBehaviour{
 	
 	Rigidbody2D rb;
 
+	GameObject modified;
+
+	public Accelerator acc;
+
+	public bool onBlock = false;
+	public bool isReady = true;
+
 	public bool dragging = false;
 	private float distance;
 
@@ -22,6 +29,16 @@ public class DragnDrop : MonoBehaviour{
 	void OnMouseUp(){
 		rb.velocity = new Vector3(0,0,0);
 		dragging = false;
+		if(onBlock == true)
+        {
+			if(modified != null) // if the accelerator is on a modifiable object when releasing the mouse button, it will become the child of said object
+            {
+				isReady = true;
+				gameObject.transform.parent = modified.transform;
+				Rigidbody2D rbModified = modified.GetComponentInParent<Rigidbody2D>();
+				acc.rbParent = rbModified;
+			}
+        }
 	}
 
 	void Update(){
@@ -29,6 +46,33 @@ public class DragnDrop : MonoBehaviour{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector3 rayPoint = ray.GetPoint(distance);
             transform.position = rayPoint;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+
+
+		if (other.tag == "Modifiable")
+		{
+			onBlock = true;
+			modified = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+
+		if (other.tag == "Modifiable")
+		{
+			onBlock = false;
+			isReady = false;
+            if (dragging) // to make sure that the accelerator was removed by the player. (and not the faulty collision system)
+            {
+				modified = null;
+				transform.parent = null;
+				acc.rbParent = null;
+			}
 		}
 	}
 }
